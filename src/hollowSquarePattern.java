df18d87b0d4690d733a55dc2f6a6cd3ab22fe115
Vinit <vinitprajapattat@gmail.com>
2026-06-02 18:37:59 +0530

    hollowSquarePattern

---
+++ src/hollowSquarePattern.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class hollowSquarePattern {
+    static void pattern(int n) {
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == n - 1 ||
+                        j == 0 || j == n - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        pattern(num);
+    }
+}
