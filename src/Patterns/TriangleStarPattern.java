a0d7faded0d6ad38fce58714632062a66f62a892
Vinit <vinitprajapattat@gmail.com>
2026-06-02 13:14:08 +0530

    TriangleStarPattern

---
+++ src/Patterns/TriangleStarPattern.java
@@ -0,0 +1,19 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class TriangleStarPattern {
+    static void pattern(int num){
+        for(int i = 0; i < num; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        pattern(num);
+    }
+}
