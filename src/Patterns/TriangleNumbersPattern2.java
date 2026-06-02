9fc0c2f7725edbf210c20fc207285773f60c29fb
Vinit <vinitprajapattat@gmail.com>
2026-06-02 13:42:50 +0530

    TriangleNumbersPattern2

---
+++ src/Patterns/TriangleNumbersPattern2.java
@@ -0,0 +1,19 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class TriangleNumbersPattern2 {
+    static void pattern(int num){
+        for(int i = 1; i <= num; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        pattern(num);
+    }
+}
