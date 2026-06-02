127d9362b6edc7d406b3e4bd3e22e9e1bc35e16b
Vinit <vinitprajapattat@gmail.com>
2026-06-02 13:24:49 +0530

    TriangleNumbersPattern

--- src/Patterns/TriangleNumbersPattern.java
+++ src/Patterns/TriangleNumbersPattern.java
@@ -1,4 +1,19 @@
 package Patterns;
 
-public class TriangleNumbersPattern {
+import java.util.Scanner;
+
+public class  TriangleNumbersPattern {
+    static void pattern(int num){
+        for(int i = 1; i <= num; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        pattern(num);
+    }
 }
