adc8b8def636cec082fcc25694b00f5f7acdf69b
Vinit <vinitprajapattat@gmail.com>
2026-06-03 14:49:45 +0530

    palindrome number

---
+++ basicMathsProgram/src/palindromeNumber.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class palindromeNumber {
+    static int reverse(int num){
+        int reverse = 0;
+        while(num > 0){
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num /= 10;
+        }
+        return reverse;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+
+        System.out.println(reverse(num) == num);
+    }
+}
