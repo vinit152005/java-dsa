5a070160cdb20c9d1f32498d381409aaa3624385
Vinit <vinitprajapattat@gmail.com>
2026-06-03 15:54:54 +0530

    prime number

---
+++ basicMathsProgram/src/primeNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class primeNumber {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        boolean isPrime = true;
+        if(num <= 1){
+            System.out.println(false);
+            return;
+        }
+        for(int i = 2; i <= Math.sqrt(num); i++){
+            if(num % i == 0){
+                System.out.println(false);
+                return;
+            }
+        }
+        System.out.println(isPrime);
+    }
+}
