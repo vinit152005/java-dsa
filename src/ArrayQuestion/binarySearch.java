b1186854bd3d95df0085f37346c51278e81bb883
Vinit <vinitprajapattat@gmail.com>
2026-06-16 12:17:03 +0530

    Binary Search

---
+++ src/ArrayQuestion/binarySearch.java
@@ -0,0 +1,32 @@
+package ArrayQuestion;
+
+import java.util.Scanner;
+
+public class binarySearch {
+
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            int[] array = {1 , 10 ,19 , 90};
+            int target = input.nextInt();
+            int ans  = binarySearch(array , target);
+            System.out.println(ans);
+
+        }
+        public static int binarySearch(int[] array , int target){
+            int start = 0;
+            int end = array.length;
+
+            while(start <= end){
+                int mid = start + (end - start)/2;
+                if(target < array[mid]){
+                    end = mid - 1;
+                }else if(target > array[mid]){
+                    start = mid + 1;
+                }else{
+                    return mid;
+                }
+            }
+            return -1;
+        }
+
+}
