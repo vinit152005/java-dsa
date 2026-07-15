a33c8df9614137777e0b211229135bc1d0173219
Vinit <vinitprajapattat@gmail.com>
2026-07-15 18:28:31 +0530

    Bubble Sorting

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -2,18 +2,27 @@ package Sorting;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {4,6,9,34,32,4};
-        for(int j = 0; j < arr.length; j++){
-            for(int i =1; i<arr.length-j; i++){
-                if(i<arr.length-1 && arr[i] > arr[i+1]){
+        int[] arr = {4, 6, 9, 34, 32, 4};
+
+        for (int j = 0; j < arr.length - 1; j++) {
+            boolean swap = false;
+
+            for (int i = 0; i < arr.length - 1 - j; i++) {
+                if (arr[i] > arr[i + 1]) {
                     int temp = arr[i];
-                    arr[i] = arr[i+1];
-                    arr[i+1] = temp;
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    swap = true;
                 }
             }
+
+            if (!swap) {
+                break;
+            }
         }
-        for(int i  =0; i < arr.length; i++){
-            System.out.print(arr[i] + " ");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
         }
     }
 }
